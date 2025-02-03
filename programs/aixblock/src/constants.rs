e8ce007cd70d2d58c22c4456213d0c822d197d22
Mahhheshh <100200105+Mahhheshh@users.noreply.github.com>
2025-02-03 13:56:58 +0530

    remove unused import

--- programs/aixblock/src/constants.rs
+++ programs/aixblock/src/constants.rs
@@ -7,8 +7,6 @@ pub const MONTHLY_UNLOCK_SUPPLY: u32 = 10_000;
 
 pub const FAIRNESS_THRESHOLD: u16 = 500;
 
-pub const COOLDOWN_PERIOD: u32 = 2592000; // (30 * 24 * 60 * 60); 30 days
-
 pub const CLAIM_WINDOW: u32 = 172800; // (2 * 24 * 60 * 60)
 
 pub const TOKEN_CLAIM_AFTER: i32 = 2419200; // 28 days
